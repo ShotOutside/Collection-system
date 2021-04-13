6e2d081c4e94d580b2c4cb83df6f82ae6c51713a
3037132737 <76683865+3037132737@users.noreply.github.com>
2021-04-13 14:04:06 +0800

    member 添加

--- Maven_Project/src/main/java/com/rj/bd/member/controller/MemberController.java
+++ Maven_Project/src/main/java/com/rj/bd/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.rj.bd.member.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,7 +32,7 @@ public class MemberController extends BaseController{
 	
 	private Map<String, Object> data;
 	
-	
+	private Member member;
 	
 	
 	@RequestMapping("/query")
@@ -46,7 +47,21 @@ public class MemberController extends BaseController{
 		data = new HashMap<String, Object>();
 		this.data = print(data, "0", "success");
 		return list;
-		
+	}
+	
+	@RequestMapping("/add")
+	public void add(HttpServletRequest request,HttpServletResponse response){
+		System.out.println("add");
+		Member member=new Member();
+		member.setM_id(UUID.randomUUID().toString());
+		member.setM_name(request.getParameter("m_name"));
+		member.setM_sex(request.getParameter("m_sex"));
+		member.setM_tel(request.getParameter("m_tel"));
+		member.setG_id(request.getParameter("g_id"));
+		System.out.println(member);
+		memberService.save(member);
+		data = new HashMap<String, Object>();
+		this.data = print(data, "0", "success");
 		
 	}
 
@@ -72,6 +87,22 @@ public class MemberController extends BaseController{
 
 	public void setData(Map<String, Object> data) {
 		this.data = data;
+	}
+
+	public IMemberService getMemberService() {
+		return memberService;
+	}
+
+	public void setMemberService(IMemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
 	}
 	
 	

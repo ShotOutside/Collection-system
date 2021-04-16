def441487b1368499d66d1ef9edbf7aef807dbdd
a1758855483 <a1758855483@163.com>
2021-04-16 14:44:58 +0800

    freemarker

--- Maven_Project/src/main/java/com/rj/bd/way/controller/WayController.java
+++ Maven_Project/src/main/java/com/rj/bd/way/controller/WayController.java
@@ -1,16 +1,11 @@
 package com.rj.bd.way.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.rj.bd.way.entity.Way;
 import com.rj.bd.way.service.IWayService;
 
 

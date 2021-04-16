21e4fcbd03cc83f8936ed41d54545efde7b72f39
a1758855483 <a1758855483@163.com>
2021-04-16 15:29:19 +0800

    fk

--- Maven_Project/src/main/java/com/rj/bd/way/controller/WayController.java
+++ Maven_Project/src/main/java/com/rj/bd/way/controller/WayController.java
@@ -1,11 +1,18 @@
 package com.rj.bd.way.controller;
 
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+
+import com.rj.bd.way.entity.Way;
 import com.rj.bd.way.service.IWayService;
 
 
@@ -17,10 +24,22 @@ public class WayController {
 	public IWayService wayService;
 	
 	@RequestMapping("/query11")
-	public ModelAndView queryFK(ModelAndView modelAndView){
-
-		modelAndView.setViewName("/list_fk");
+	public String queryFK(HttpServletRequest request,ModelMap map){
+		System.out.println("UserController:queryFK()");
 		
-		return modelAndView;
+	     List<Way> list = wayService.findAll();
+		
+		System.out.println("--------------->"+list.size());
+		
+		//request.setAttribute("list", list);
+		map.addAttribute("list", list);
+		String path = request.getScheme() + "://" + request.getServerName() + ":"
+				+ request.getServerPort() + request.getContextPath() + "/";
+		map.addAttribute("path", path);
+		for (Way way : list) {
+			System.out.println(way.getW_id()+"\t"+way.getW_name());
+		}
+		
+		return "list_fk";
 	}
 }
---
+++ Maven_Project/src/main/webapp/WEB-INF/way/add.ftl
@@ -0,0 +1,8 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>User模块的FK查询展示页面</title></head>
+<body>
+hello 
+</body>
+</html>
